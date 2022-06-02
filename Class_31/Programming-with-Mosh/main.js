eb3e7531a758bc4a82be6da5a68eb25de89e5656
Boobeh123 <boobeh321@gmail.com>
2022-06-01 14:32:08 -1000

    Add inheritance, polymorphism, fundamental concepts

--- Class_31/Programming-with-Mosh/main.js
+++ Class_31/Programming-with-Mosh/main.js
@@ -92,11 +92,197 @@ Fewer properties and methods are easier to use than several.
 
 
 /**************************************************************
-* INHERITANCE - Video examples - 6:29
+* INHERITANCE - Video examples - 6:29 ~ 7:14
+***************************************************************/
+/*
+Inheritance is a mechanism that allows you to eliminate redundant code.
+
+HTML-elements such as text boxes, drop-down lists, and check boxes
+all share similar properties and methods.
+
+Instead of redefining all these properties & methods for every type of HTMLElement,
+we can define them once in a generic object, HTMLElement.
+
+Other objects can then inherit these properties and methods.
+*/
+/**************************************************************
+* INHERITANCE - Video examples - 6:29 ~ 7:14
+***************************************************************/
+
+
+
+
+/**************************************************************
+* POLYMORPHISM - Video examples - 7:15 ~ 8:10
+***************************************************************/
+/*
+Polymorphism means many forms.
+In OOP, polymorphism is a technique that allows you to get rid of
+long if/else statements or switch/case statements.
+
+This is referencing the HTML-elements from inheritance:
+The text box, drop-down list, and check box objects should have the ability
+to be rendered on the page.
+
+To render objects in a procedural programming way, 
+the code would look like a switch statement with several cases.
+
+To render objects in an OOP way,
+you would implement a render method in each object.
+*/
+/**************************************************************
+* POLYMORPHISM - Video examples - 7:15 ~ 8:10
+***************************************************************/
+
+
+
+
+/**************************************************************
+* BENEFITS OF OOP - Video examples - 8:11 ~ 8:53
+***************************************************************/
+/*
+Encapsulation - We group related variables & functions together.
+Grouping these together reduces the complexity. 
+Objects can now be reused in different parts of a program.
+
+Abstraction - We hide the complex details and show the essentials.
+Incorporating abstraction also reduces the complexity and
+isolates the impact of changes in the code.
+
+Inheritance - We eliminate redundant code.
+
+Polymorphism - Refactor switch/case statements.
+*/
+/**************************************************************
+* BENEFITS OF OOP - Video examples - 8:11 ~ 8:53
+***************************************************************/
+
+
+
+
+/**************************************************************
+* SET UP DEVELOPER ENVIRONMENT - 8:54 ~ 11:21
+***************************************************************/
+/*
+ * Download VSCode (or any text editor) 
+ * Download Live Server plugin
+ * Create index.html
+ * Setup html template (html:5 or bang tab)
+ * Link script src
+ * Create main.js
+ */
+/**************************************************************
+* SET UP DEVELOPER ENVIRONMENT - 8:54 ~ 11:21
+***************************************************************/
+
+
+
+
+/**************************************************************
+* OBJECT LITERALS - Video examples - 11:53 ~ 14:57
+***************************************************************/
+/*
+Objects are essentially a collection of key value pairs
+Property is used to hold values
+Method is used to define some logic
+
+Literal notation is a simple way to define an object.
+*/
+
+// Creating an object with literal notation
+const circle = {
+    // Properties
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+
+    // Method
+    draw() {
+        console.log('Created with literal notation');
+    }
+};
+
+// Accessing circle object with dot notation
+circle.draw();      // Output -> Created with literal notation
+/**************************************************************
+* OBJECT LITERALS - Video examples - 11:53 ~ 14:57
+***************************************************************/
+
+
+
+
+/**************************************************************
+* FACTORIES - Video examples - 14:58 ~ 17:50
+***************************************************************/
+/*
+Imagine we want to create another circle object.
+Using literal notation is not a good way to duplicate objects.
+Using a factory (or constructor function) is the solution for this.
+
+If we return an object in a function, then the function is referred as a factory.
+*/
+
+function CreateCircles(radius) {
+    return {
+        radius,     // If key & value share same name, we can remove the value
+        draw() {
+            console.log('Created with a factory function');
+        }
+    }
+}
+// Call factory function to create object
+const circle2 = new CreateCircles(2);
+circle2.draw();     // Output -> Created with a factory function
+/**************************************************************
+* FACTORIES - Video examples - 14:58 ~ 17:50
+***************************************************************/
+
+
+
+
+/**************************************************************
+* CONSTRUCTORS - Video examples - 17:51 ~ 23:27
+***************************************************************/
+/*
+Imagine we want to create another circle object.
+Using literal notation is not a good way to duplicate objects.
+Using a constructor (or factory function) is the solution for this.
+
+If we use this keyword and new operator in a function, then the function
+is referred as a constructor. 
+
+The this keyword is a reference to the object that is executing the code.
+
+3 Things happen when using the new operator to call a function
+- An empty object is created
+- This keyword points to that object
+- Return that object from this function
+*/
+
+function CreateCircles2(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('Created with a constructor function');
+    }
+}
+// Call constructor function to create object
+const circle3 = new CreateCircles2(2);
+circle3.draw();     // Output -> Created with a constructor function
+/**************************************************************
+* CONSTRUCTORS - Video examples - 17:51 ~ 23:27
+***************************************************************/
+
+
+
+
+/**************************************************************
+* CONSTRUCTOR PROPERTY - Video examples - 23:28 ~
 ***************************************************************/
 /*
 
 */
 /**************************************************************
-* INHERITANCE - Video examples - 6:29
+* CONSTRUCTOR PROPERTY - Video examples - 23:28 ~
 ***************************************************************/
