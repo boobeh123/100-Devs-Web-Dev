b9221c7820cbf4599c0383e4f35293a81a0e7910
Boobeh123 <boobeh321@gmail.com>
2022-08-22 17:16:12 -1000

    Add Historical context, JS Old-school way, Package manager, Module bundler (4/7)

--- Class_35/Modern-JavaScript-For-Dinosaurs/main.js
+++ Class_35/Modern-JavaScript-For-Dinosaurs/main.js
@@ -1,206 +1,179 @@
 /**************************************************************
-* 
+* Introduction: JavaScript's historical context
 ***************************************************************/
 /*
+Learning modern JavaScript is tough if you haven’t been there 
+since the beginning. 
+The ecosystem is growing and changing so rapidly that it’s 
+hard to understand the problems that different tools are trying to solve. 
+
+The goal of this article is to provide a historical context of how JavaScript 
+tools have evolved to what they are today. 
+
+We’ll start from the beginning and build an example website like 
+the dinosaurs did — no tools, just plain HTML and JavaScript. 
+Then we’ll introduce different tools incrementally to see the problems 
+that they solve one at a time. 
+
+With this historical context, 
+you’ll be better able to learn and adapt to the ever-changing JavaScript 
+landscape going forward.
+*/
+/**************************************************************
+* Introduction: JavaScript's historical context
+***************************************************************/
+
+
+ 
+
+/**************************************************************
+* Using JavaScript the "old-school" way
+***************************************************************/
+/*
+------------------------------------------------------------------------
+*** Note from me: I have created a separate folder under the name:  ***
+*** "old-school-example" for this section. The notes remain here.   ***
+------------------------------------------------------------------------
+Creating an “old-school” website using HTML and JavaScript, 
+involves manually downloading and linking files. 
+
+If you wanted to use a library like moment.js,
+we can add moment.js to our website by downloading the moment.min.js file 
+in the same directory and include it in our index.html file.
+
+Loading moment.min.js before index.js, defines a global variable 
+allowing moment functions available to be used inside the index.js file.
+
+Using libraries required to find & download new versions of the library
+every time it would update.
+*/
+/**************************************************************
+* Using JavaScript the "old-school" way
+***************************************************************/
+
+
+
+
+/**************************************************************
+* Using a JavaScript package manager (npm)
+***************************************************************/
+/*
+JavaScript Package Managers automated the process of downloading & upgrading 
+libraries from a repository around 2010. 
+Bower was arguably the most popular in 2013 and npm around 2015. 
+npm was made specifically for node.js.
+
+Using package managers generally involves using the command line.
+Knowing how to use the command line is an important part of modern JavaScript.
+
+package.json is a configuration file that npm uses to save all project 
+information. 
+
+Installing the a JavaScript package using the npm command does two things:
+* Downloads all the code from the package into a folder called node_modules. 
+* Automatically modifies the package.json file to keep track of a library 
+as a project dependency.
+
+The package.json file can be shared with other developers to install the 
+required packages.
+*/
+/**************************************************************
+* Using a JavaScript package manager (npm)
+***************************************************************/
+
+
+
+
+/**************************************************************
+* Using a JavaScript module bundler (webpack)
+***************************************************************/
+/*
+Most programming languages provide a way to import code from one file 
+into another. 
+JavaScript was designed to run in the browser with no access to the 
+file system of the client’s computer. (security reasons)
+
+Organizing JavaScript code in multiple files required each file to be 
+loaded with global variables.
+(An example of this from chapter: "Using JavaScript the old-school way")
+
+CommonJS started in 2009 & had a goal of specifying an ecosystem 
+for JavaScript outside the browser. 
+CommonJS would allow JavaScript to import and export code across files, 
+without resorting to global variables. 
+The implementation of CommonJS modules is node.js.
+
+Node.js is a server side language with access to the computer’s file system. 
+Node.js knows the location of each npm module path.
+
+Using the require() method in the browser results in an error.
+The browser doesn’t have access to the file system.
+Loading files has to be done dynamically, either synchronously or asynchronously.
+
+A JavaScript module bundler is a tool that gets around this problem 
+with a build step to create a final output that is browser compatible.
+
+Browserify is a module bundler released in 2011 & pioneered the usage 
+of node.js style require() statements on the frontend.
+Webpack is another module bundler that became the more widely used around 2015.
+The React frontend framework used webpack's various features.
+
+The webpack module bundler allows us to use the require() method in the browser.
+Running the webpack tool starts with the index.js file, 
+finds require() statements, replaces it with code that creates a single output file. 
+The --mode=development argument is to keep the JavaScript readable.
+
+*** Note from me: If you receive an error about webpack.ps1 cannot be loaded because running scripts is disabled on this system ***
+*** Solution: https://stackoverflow.com/questions/41117421/ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system
+
+Webpack's output file can be found in the dist directory as a main.js file.
+Set this main.js file as a script src in the HTML instead of index.js.
+The webpack command must be called each time a change occurs in index.js
+
+We can create a new js file named webpack.config.js in the root directory of the project.
+Webpack can read options from this config file.
+With this config file, we don't need to start with the index.js or provide
+the --mode=development argument when calling the webpack command.
+
+Using a module bundler means we no longer have to load scripts with global variables. 
+We can add new JavaScript libraries using require statements.
+Having a single JavaScript bundle file is often better for performance. 
+*/
+/**************************************************************
+* Using a JavaScript module bundler (webpack)
+***************************************************************/
+
+
+
+
+/**************************************************************
+* Transpiling code for new language features (babel)
+***************************************************************/
+/*
+Transpiling code means converting the code in one language to code in another 
+similar language. 
+
+Browsers are slow to add new features. New languages were created with 
+experimental features that transpile to browser compatible languages.
+
+For CSS, there’s Sass, Less, and Stylus, to name a few. 
+For JavaScript, the most popular transpiler for a while was CoffeeScript, 
+whereas nowadays most people use babel or TypeScript. 
+
+CoffeeScript is a language focused on improving JavaScript by significantly 
+changing the language — optional parentheses, significant whitespace, etc. 
+
+Babel is not a new language but a transpiler that transpiles next generation 
+JavaScript with features not yet available to all browsers to older more 
+compatible JavaScript (ES5). 
+
+Typescript is a language that is essentially identical to next generation 
+JavaScript, but also adds optional static typing. 
+
 
 */
 /**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
+* Transpiling code for new language features (babel)
 ***************************************************************/
 
 
@@ -228,33 +201,3 @@
 /**************************************************************
 * 
 ***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
-/**************************************************************
-* 
-***************************************************************/
-/*
-
-*/
-/**************************************************************
-* 
-***************************************************************/
-
-
-
-
--- Class_35/Modern-JavaScript-For-Dinosaurs/readME.md
+++ Class_35/Modern-JavaScript-For-Dinosaurs/readME.md
@@ -1,10 +1,42 @@
 ## Summary:
 The concepts covered in this reading:
 
-* 
-
+* JavaScript ecosystem:
+    * Constantly growing & rapidly changing.
+    * Understanding the problems that tools tried to solve.
+* Using JavaScript libraries:
+    * Manually downloading & linking these libraries.
+    * Defining global variables to allow files to be used in other files.
+* Using JavaScript package managers:
+    * Automated the process of downloading & upgrading libraries.
+    * Involves using the command line.
+    * package.json is a configuration file that npm uses to save all project information. 
+        * This file can be shared with other developers to install required packages.
+    * Installing a JavaScript package does two things:
+        * Downloads all the code from the package into a folder called node_modules. 
+        * Automatically modifies the package.json file to keep track of a library as a project dependency.
+* Using a JavaScript module bundler:
+    * JavaScript was designed to run in the browser with no access to the file system of the client’s computer. (security reasons)
+    * Organizing JavaScript code in multiple files required each file to be loaded with global variables.
+    * CommonJS allowed JavaScript to import & export code across files without resorting to global variables.
+    * Node.js is a server-side language with access to the computer’s file system. 
+    * Allows the require() method to be used in the browser.
+        * Creates a build step to create a final output.
+* Transpiling code - Converting the code in one language to code in another similar language.
+    
 ## Lessons learned:
+To be continued..
+
+This reading covers the JavaScript's historical context to understand the problems that tools were trying to solve. 
+I learned that a file had to define global variables so that other files may access the first file. 
+Libraries had to be manually updated before package managers automated this process. Using the a package manager also meant that developers had to use the command line.
+
+JavaScript was originally designed to run in the browser and did not allow access to the file system of a computer due to security reasons. 
+While learning how to use the webpack module bundler, I ran into a Windows-specific error that disabled my machine from running scripts. The solution is linked in the source below.
 
 
 ## Source:
 https://medium.com/the-node-js-collection/modern-javascript-explained-for-dinosaurs-f695e9747b70
+
+Error Fix: webpack.ps1 cannot be loaded because running scripts is disabled on this system
+Solution: https://stackoverflow.com/questions/41117421/ps1-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system
