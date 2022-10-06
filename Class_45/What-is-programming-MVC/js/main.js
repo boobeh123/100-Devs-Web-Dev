56ad43637aa308f7879e591b84bc13ea01b692d3
Boobeh123 <boobeh321@gmail.com>
2022-10-06 11:40:02 -1000

    Notes

--- Class_45/What-is-programming-MVC/js/main.js
+++ Class_45/What-is-programming-MVC/js/main.js
@@ -1,33 +1,71 @@
 /**************************************************************
-* 
+* Introduction 0:00 ~ 5:20
 ***************************************************************/
 /*
+
+* Model-View-Controller is an architectural paradigm.
+    * The structure of web development will be built in a certain way.
+    * This structure will process information in a certain way.
+    * A way to structure an application.
+* MVC was created in 1979.
+
 
 */
 /**************************************************************
-* 
+* Introduction 0:00 ~ 5:20
 ***************************************************************/
 
 
 
 
 /**************************************************************
-* 
+* What is MVC? 5:21 ~ 8:00
 ***************************************************************/
 /*
+
+M - Model / V - View / C - Controller
+
+* All 3 are required, missing one would cause the structure to crumble.
+* Models deal with storage of information.
+    * Storage of information deals with login or authentication. 
+* MVC prevents us from repeating ourselves
+    * If an element needs to be changed it will only need to be changed in one place.
+* MVC helps create a solid structure around web applications.
 
 */
 /**************************************************************
-* 
+* What is MVC? 5:21 ~ 8:00
 ***************************************************************/
 
 
 
 
 /**************************************************************
-* 
+* The flow: 8:01 ~ 
 ***************************************************************/
 /*
+
+* The model is the database.
+* The view is the client.
+* The controller is the server.
+
+* The Client is the user viewing the internet.
+    * The client will use browsers send requests.
+    * The client-side will render HTML/CSS/JS.
+        * The client-side code happens in the browser.
+
+* A client will send a request to a server.
+    * Servers are usually linux or windows based.
+    * Servers run server-side code.
+    * The servers do not store any information.
+    * The server will interact with the database 
+        * The db will search its files/documents 
+            * The db sends a request back to the server.
+            * The server sends a response with HTML/CSS back to the client.
+                * The client's web browser will process the HTML/CSS & render it.
+    
+* The database will store information.
+    * Databases can be relational or non-relational.
 
 */
 /**************************************************************
