b916ba662d4ce8997e07c993556ebe20a4095ddc
Boobeh123 <boobeh321@gmail.com>
2022-12-16 16:15:25 -1000

    Add plugin name to notes

--- Class_58/EggHead-The-Beginners-Guide-To-React/08 - Validate Custom React Component Props with PropTypes/notes.js
+++ Class_58/EggHead-The-Beginners-Guide-To-React/08 - Validate Custom React Component Props with PropTypes/notes.js
@@ -6,6 +6,9 @@
 The React team developed PropTypes which allows you 
 to add run time validation of the props that are passed to your components. 
 
+A Babel plugin that will remove the prop-types from your source code for production. 
+That plugin is called Babel-plugin-transform-remove-prop-types.
+
 */
 /**************************************************************
 * Validate Custom React Component Props with PropTypes
