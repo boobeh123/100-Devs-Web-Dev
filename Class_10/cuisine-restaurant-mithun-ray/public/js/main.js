8bbeaaddd1fe28aac1584d0141d59031ad289431
Boobeh123 <boobeh321@gmail.com>
2023-01-15 11:18:02 -1000

    Add search bar functionality

--- Class_10/cuisine-restaurant-mithun-ray/public/js/main.js
+++ Class_10/cuisine-restaurant-mithun-ray/public/js/main.js
@@ -13,3 +13,21 @@ const carouselInstances = M.Carousel.init(carousel, {
 let carouselIntervalId = setInterval(() => {
     carouselInstances[0].next();
 }, 3000);
+
+const searchForm = document.querySelector('#search-form')
+
+searchForm.addEventListener('submit', searchBar);
+
+function searchBar(e) {
+    e.preventDefault()
+    const searchInput = document.querySelector('#search')
+    let allElements = document.querySelectorAll('*');
+    let inputValue = searchInput.value.toLowerCase();
+
+    for (let i = 0; i < allElements.length; i++) {
+        if (allElements[i].textContent.toLowerCase().includes(inputValue)) {
+            allElements[i].scrollIntoView({ behavior: 'smooth' });
+        } 
+    }
+    searchInput.value = ''; 
+}
